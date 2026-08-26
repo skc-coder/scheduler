7ee3d20bcb12f894d295d647a322ac8bff1a56ba
skc-coder <84ya+github@Proton.me>
2026-08-26 14:03:44 +0530

    Implemented logger header and tested it. It is fun

---
+++ include/Logger.hpp
@@ -0,0 +1,54 @@
+// By Shekhar Chaurasiya
+// 26 Aug 2026
+
+#pragma once
+#include <iostream>
+#include <string_view>
+
+namespace Aryabhata::Logger{
+    enum class LogLevel {
+        INFO,
+        WARN,
+        ERROR,
+        SUCCESS
+    };
+
+    namespace COLORS {
+        constexpr std::string_view RESET = "\033[0m";
+        constexpr std::string_view RED= "\033[31m";
+
+        constexpr std::string_view GREEN= "\033[32m";
+
+
+        constexpr std::string_view YELLOW= "\033[33m";
+
+
+        constexpr std::string_view CYAN="\033[36m";
+
+
+    };
+
+
+
+    inline void log(LogLevel level, std::string_view msg){
+        switch (level) {
+            case LogLevel::INFO:
+                std::cout << COLORS::CYAN <<msg<<COLORS::RESET<<'\n';
+                break;
+            case LogLevel::WARN:
+                std::cout << COLORS::YELLOW <<msg<<COLORS::RESET<<'\n';
+                break;
+
+            case LogLevel::ERROR:
+                std::cout << COLORS::RED <<msg<<COLORS::RESET<<'\n';
+                break;
+
+            case LogLevel::SUCCESS:
+                std::cout << COLORS::GREEN <<msg<<COLORS::RESET<<'\n';
+                break;
+
+
+        }
+    };
+
+}
